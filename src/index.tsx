5e7386ecb1acb7bd9900977416eab76c73a99c23
MYD <2078546589@qq.com>
2025-05-20 17:00:23 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,24 @@ import {Context, h, Schema, Logger} from 'koishi'
 export const name = 'vvsays'
 
 export interface Config {
-  picNumber: number
+  picNumber: number,
+  searchAPI: string
 }
 
 export const Config: Schema<Config> = Schema.object({
   picNumber: Schema.number()
     .default(1)
     .min(0).max(10).step(1)
-    .description('查询并返回的VV语录表情包数量')
+    .description('查询并返回的VV语录表情包数量'),
+  searchAPI: Schema.string()
+    .default("https://api.zvv.quest/search")
+    .description('VVQuest兼容API接口')
 })
 
 export function apply(ctx: Context, config: Config) {
   const logger = new Logger('vvsays')
   ctx.command('zvv <message>').alias('维为').action((_, message) => {
-    ctx.http.get(`https://api.zvv.quest/search`, {
+    ctx.http.get(config.searchAPI, {
       params: {
         q: message,
         n: config.picNumber
